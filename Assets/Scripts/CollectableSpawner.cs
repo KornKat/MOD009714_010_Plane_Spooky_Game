8232828f57a9e7b205182ff6537e9ea1221cc967
Kornelijus Katinas <KK918@student.aru.ac.uk>
2025-03-17 16:05:53 +0000

    Added Fog A few Adjustments to the map

--- Assets/Scripts/CollectableSpawner.cs
+++ Assets/Scripts/CollectableSpawner.cs
@@ -5,7 +5,7 @@ public class CollectableSpawner : MonoBehaviour
     public GameObject collectablePrefab; // The collectable prefab to spawn
     public int numberOfCollectables = 10; // Number of collectables to spawn
     public float minRadius = 90f; // Minimum distance from the center
-    public float maxRadius = 150f; // Maximum distance from the center
+    public float maxRadius = 140f; // Maximum distance from the center
 
     void Start()
     {
